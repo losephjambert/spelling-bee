86fb3b2953b316795b9098ba8ca37beda7e5198d
Joe Lambert <josephc.lambert@gmail.com>
2019-11-03 17:22:51 -0800

    stubs letters array. makes cells visually smaller and identifies the center cell.

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,32 +1,34 @@
 <script>
   export let letter = '';
   export let handleClick;
+  export let center;
 </script>
 
 <style>
-  button {
-    background-color: transparent;
-    outline: none;
-    border: none;
+  svg.hive-cell {
+    width: 75px;
   }
-  svg.hive-cell.outer {
+  svg.hive-cell.center > polygon.cell-fill {
+    fill: #f8cd05;
   }
   polygon.cell-fill {
-    fill: hsl(0, 0%, 90%);
+    fill: #e6e6e6;
   }
   text.cell-letter {
     text-transform: uppercase;
+    text-anchor: middle;
   }
 </style>
 
-<button on:click={e => handleClick(e, letter)}>
-  <svg class="hive-cell outer" viewBox="0 0 120 103.92304845413263">
-    <polygon
-      class="cell-fill"
-      points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263
-      30,103.92304845413263"
-      stroke="white"
-      stroke-width="7.5" />
-    <text class="cell-letter" x="50%" y="50%" dy="10.75%">{letter}</text>
-  </svg>
-</button>
+<svg
+  class="hive-cell {center ? 'center' : 'outer'}"
+  viewBox="0 0 120 103.92304845413263"
+  on:click={e => handleClick(e, letter)}>
+  <polygon
+    class="cell-fill"
+    points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263
+    30,103.92304845413263"
+    stroke="white"
+    stroke-width="7.5" />
+  <text class="cell-letter" x="50%" y="50%" dy="10.75%">{letter}</text>
+</svg>
--- src/components/Hive.svelte
+++ src/components/Hive.svelte
@@ -1,6 +1,9 @@
 <script>
   import Cell from './Cell.svelte';
 
+  // cell letter stub data
+  const letters = ['f', 'i', 'n', 'r', 'c', 'h', 'o'];
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log('submit word', e.target);
@@ -12,11 +15,21 @@
   };
 </script>
 
+<style>
+  section.hive {
+    position: relative;
+  }
+</style>
+
 <section>
   <form on:submit={handleSubmit}>
     <input type="text" name="word" id="word" />
   </form>
-  <Cell {handleClick} letter={'j'} />
+  <section class="hive">
+    {#each letters as letter, i}
+      <Cell {handleClick} {letter} center={i === letters.length - 1} />
+    {/each}
+  </section>
   <button>Delete</button>
   <button>Refresh</button>
   <button>Enter</button>
